5a1a51fd3f41ef7a8144a80f9e86bfba279e43bf
nishchalnishant <40529054+nishchalnishant@users.noreply.github.com>
2021-10-23 20:14:48 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -70,64 +70,64 @@ result=pd.DataFrame(columns=['Station','co_predicted','co_actual','co_diff',
 
 #predicting values for next hour
 
-if(files):
-
-    for i in (files):
-        path=os.path.join('Data',i)
-        df=pd.read_excel(path)
-        
-        # avg of last 10 data 
-        co_predicted=df.iloc[-5:-1]['co'].mean()
-        no2_predicted=df.iloc[-5:-1]['no2'].mean()
-        o3_predicted=df.iloc[-5:-1]['o3'].mean()
-        pm10_predicted=df.iloc[-5:-1]['pm10'].mean()
-        pm25_predicted=df.iloc[-5:-1]['pm25'].mean()
-        so2_predicted=df.iloc[-5:-1]['so2'].mean()
-
-        co_actual=df.iloc[-1:]['co'].mean()
-        no2_actual=df.iloc[-1:]['no2'].mean()
-        o3_actual=df.iloc[-1:]['o3'].mean()
-        pm10_actual=df.iloc[-1:]['pm10'].mean()
-        pm25_actual=df.iloc[-1:]['pm25'].mean()
-        so2_actual=df.iloc[-1:]['so2'].mean()
-
-        result=result.append({
-                'Station':i[:-5],
-                'co_predicted':co_predicted,
-                'co_actual':co_actual,
-                'co_diff':(co_predicted-co_actual),
-                'no2_predicted':no2_predicted,
-                'no2_actual':no2_actual,
-                'no2_diff':(no2_predicted-no2_actual),
-                'o3_predicted':o3_predicted,
-                'o3_actual':o3_actual,
-                'o3_differnce':(o3_predicted-o3_actual),
-                'pm10_predicted':pm10_predicted,
-                'pm10_actual':pm10_actual,
-                'pm10_diff':(pm10_predicted-pm10_actual),
-                'pm25_predicted':pm25_predicted,
-                'pm25_actual':pm25_actual,
-                'pm25_diff':(pm25_predicted-pm25_actual),
-                'so2_predicted':so2_predicted,
-                'so2_actual':so2_actual,
-                'so2_diff':(so2_predicted-so2_actual)},ignore_index=True)
 
 
+for i in (files):
+    path=os.path.join('Data',i)
+    df=pd.read_excel(path)
+    
+    # avg of last 10 data 
+    co_predicted=df.iloc[-5:-1]['co'].mean()
+    no2_predicted=df.iloc[-5:-1]['no2'].mean()
+    o3_predicted=df.iloc[-5:-1]['o3'].mean()
+    pm10_predicted=df.iloc[-5:-1]['pm10'].mean()
+    pm25_predicted=df.iloc[-5:-1]['pm25'].mean()
+    so2_predicted=df.iloc[-5:-1]['so2'].mean()
 
-    last_hour=result[['Station','co_actual','no2_actual','o3_actual','pm10_actual','pm25_actual','so2_actual']]
-        
-    last_hour = last_hour.melt('Station', var_name='cols', value_name='vals')
+    co_actual=df.iloc[-1:]['co'].mean()
+    no2_actual=df.iloc[-1:]['no2'].mean()
+    o3_actual=df.iloc[-1:]['o3'].mean()
+    pm10_actual=df.iloc[-1:]['pm10'].mean()
+    pm25_actual=df.iloc[-1:]['pm25'].mean()
+    so2_actual=df.iloc[-1:]['so2'].mean()
 
-    last_hour_graph = sns.catplot(x="Station", y="vals", hue='cols', data=last_hour, kind='point',height=5, aspect=11.7/8.27)
-        
-    last_hour_graph.savefig("last_hour.png")
+    result=result.append({
+            'Station':i[:-5],
+            'co_predicted':co_predicted,
+            'co_actual':co_actual,
+            'co_diff':(co_predicted-co_actual),
+            'no2_predicted':no2_predicted,
+            'no2_actual':no2_actual,
+            'no2_diff':(no2_predicted-no2_actual),
+            'o3_predicted':o3_predicted,
+            'o3_actual':o3_actual,
+            'o3_differnce':(o3_predicted-o3_actual),
+            'pm10_predicted':pm10_predicted,
+            'pm10_actual':pm10_actual,
+            'pm10_diff':(pm10_predicted-pm10_actual),
+            'pm25_predicted':pm25_predicted,
+            'pm25_actual':pm25_actual,
+            'pm25_diff':(pm25_predicted-pm25_actual),
+            'so2_predicted':so2_predicted,
+            'so2_actual':so2_actual,
+            'so2_diff':(so2_predicted-so2_actual)},ignore_index=True)
 
-    diff_df=result[['Station','co_diff','no2_diff','o3_differnce','pm10_diff','pm25_diff','so2_diff']]
 
-    diff_df = diff_df.melt('Station', var_name='cols', value_name='vals')
 
-    g = sns.catplot(x="Station", y="vals", hue='cols', data=diff_df, kind='point',height=5, aspect=11.7/8.27)
+last_hour=result[['Station','co_actual','no2_actual','o3_actual','pm10_actual','pm25_actual','so2_actual']]
+    
+last_hour = last_hour.melt('Station', var_name='cols', value_name='vals')
 
-    g.savefig("difference.png")
+last_hour_graph = sns.catplot(x="Station", y="vals", hue='cols', data=last_hour, kind='point',height=5, aspect=11.7/8.27)
+    
+last_hour_graph.savefig("last_hour.png")
+
+diff_df=result[['Station','co_diff','no2_diff','o3_differnce','pm10_diff','pm25_diff','so2_diff']]
+
+diff_df = diff_df.melt('Station', var_name='cols', value_name='vals')
+
+g = sns.catplot(x="Station", y="vals", hue='cols', data=diff_df, kind='point',height=5, aspect=11.7/8.27)
+
+g.savefig("difference.png")
 
 

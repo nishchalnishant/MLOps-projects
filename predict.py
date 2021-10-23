a5f0a9f39d74c76bd80290a31434a0499bcdb7dc
nishchalnishant <40529054+nishchalnishant@users.noreply.github.com>
2021-10-23 21:45:12 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,58 +2,6 @@ import pandas as pd
 import numpy as np
 import os
 import seaborn as sns
-import requests
-
-
-
-url_samanpura='https://docs.google.com/spreadsheets/d/1fmkYgHTWwEbdgWxQgyRPG5g1ag-iLb3Emnuo-SRrno8/export?format=xlsx&gid=0'
-url_drm_office='https://docs.google.com/spreadsheets/d/1fmkYgHTWwEbdgWxQgyRPG5g1ag-iLb3Emnuo-SRrno8/export?format=xlsx&gid=1825925003'
-url_govt_high_school='https://docs.google.com/spreadsheets/d/1fmkYgHTWwEbdgWxQgyRPG5g1ag-iLb3Emnuo-SRrno8/export?format=xlsx&gid=1321311482'
-url_muradapur='https://docs.google.com/spreadsheets/d/1fmkYgHTWwEbdgWxQgyRPG5g1ag-iLb3Emnuo-SRrno8/export?format=xlsx&gid=409038259'
-url_rajbansi_nagar='https://docs.google.com/spreadsheets/d/1fmkYgHTWwEbdgWxQgyRPG5g1ag-iLb3Emnuo-SRrno8/export?format=xlsx&gid=2107345793'
-
-#creating the Data directory
-os.mkdir('Data')
-
-
-#requesting the samanpural file 
-r = requests.get(url_samanpura, allow_redirects=True)
-
-temp=open('Data/samanpura.xlsx', 'wb')
-temp.write(r.content)
-temp.close()
-
-#requesting the samanpural file 
-r = requests.get(url_drm_office, allow_redirects=True)
-
-temp=open('Data/Drm_office.xlsx', 'wb')
-temp.write(r.content)
-temp.close()
-
-#requesting the samanpural file 
-r = requests.get(url_govt_high_school, allow_redirects=True)
-
-temp=open('Data/govt_high_school.xlsx', 'wb')
-temp.write(r.content)
-temp.close()
-
-#requesting the samanpural file 
-r = requests.get(url_muradapur, allow_redirects=True)
-
-temp=open('Data/murdapur.xlsx', 'wb')
-temp.write(r.content)
-temp.close()
-
-#requesting the samanpural file 
-r = requests.get(url_rajbansi_nagar, allow_redirects=True)
-
-temp=open('Data/rajbansi_nagar.xlsx', 'wb')
-temp.write(r.content)
-temp.close()
-
-
-
-
 
 """
 we will loop through each xlsx file in the Data folder and try to predict the next pollutant value  
